717617bec9e8bf4cf9f8f71180a64332b860de95
Tisa Biswal <tisabiswl29@gmail.com>
2026-03-12 02:34:59 +0530

    Added solution for Question 1

---
+++ Q1.java
@@ -0,0 +1,14 @@
+public class Q1
+{
+    public static void main(String[] args)
+    {
+        int num = 5;
+
+        if (num > 0)
+            System.out.println("The number is positive");
+        else if (num < 0)
+            System.out.println("The number is negative");
+        else
+            System.out.println("Zero");
+    }
+}
