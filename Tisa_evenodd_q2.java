187e44caac0eddcce8817fb2e0d878bd5cda6ff2
Tisa Biswal <tisabiswl29@gmail.com>
2026-03-13 01:34:37 +0530

    Added second code

---
+++ Tisa_evenodd_q2.java
@@ -0,0 +1,11 @@
+public class Tisa_evenodd_q2 {
+    public static void main(String[] args){
+        int a=5;
+        if(a%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+    }
+}
